42fa7d453d1b28329fd7d6bb83edd4c96090e453
TraciK <70920574+TraciK@users.noreply.github.com>
2025-07-30 15:32:58 +0000

    saving

--- dice.rb
+++ dice.rb
@@ -20,7 +20,7 @@ get("/dice/2/6") do
   
   @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
   
- erb(:two_six, { :layout => :wrapper })
+ erb(:two_six)
 end
 
 get("/dice/2/10") do
@@ -30,7 +30,7 @@ get("/dice/2/10") do
 
   @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}"
 
-  erb(:two_ten, { :layout => :wrapper })
+  erb(:two_ten)
 end
 
 get("/dice/1/20") do
@@ -38,7 +38,7 @@ get("/dice/1/20") do
   
   @outcome = "You rolled a #{die}"
  
-  erb(:one_twenty, { :layout => :wrapper })
+  erb(:one_twenty)
 end
 
 get("/dice/5/4") do
@@ -51,5 +51,5 @@ get("/dice/5/4") do
 
   @outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, a #{fourth_die}, and a #{fifth_die} for a total of #{sum}"
   
- erb(:five_four, { :layout => :wrapper })
+ erb(:five_four)
 end
